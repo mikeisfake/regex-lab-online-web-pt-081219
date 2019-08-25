2ef7c18948e56f42b7cb6faed6e0d7a98b119a2c
Mike Cooper <mikescooper@icloud.com>
2019-08-25 15:47:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiou]/)
-    true 
-  else
-    false 
-  end 
+  word.match(/^[aeiouAEIOU]\w+/) ? true : false 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
